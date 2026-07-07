901eb52d67144bfad0c82ac3d1b984ce9bb00bc6
Vasantham Kalyan <156561281+kalyan-rama@users.noreply.github.com>
2026-07-07 06:58:30 +0530

    Update API_URL to use HTTPS

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,2 +1,2 @@
-const API_URL = (import.meta as any).env?.VITE_API_URL || (typeof window !== 'undefined' ? window.location.origin : 'godhara-backend-production.up.railway.app');
+const API_URL = (import.meta as any).env?.VITE_API_URL || 'https://godhara-backend-production.up.railway.app';
 export default API_URL;
